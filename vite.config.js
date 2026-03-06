2fafc5e6c673ed2633ceaff4e39a83451ca31180
Arthurdavin <davinchhayml@gmail.com>
2026-03-06 20:00:02 +0700

    new

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-    
-  ],
-  // server: {
-  //   port: 5173,
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://khdemy.anajak-khmer.site",
-  //       changeOrigin: true,
-  //       secure: false,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
-  // },
-  
-})
 
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor libraries into separate chunks
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'redux-vendor': ['@reduxjs/toolkit', 'react-redux'],
+        }
+      }
+    }
+  }
+})
